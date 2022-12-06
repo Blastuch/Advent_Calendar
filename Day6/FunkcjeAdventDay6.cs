6c3422e9e3ec5493fb40b2b4f19cfc5e690e118f
Blastuch <87999681+Blastuch@users.noreply.github.com>
2022-12-06 15:27:18 +0100

    Add files via upload

---
+++ Day6/FunkcjeAdventDay6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class FunkcjeAdventDay6
+{
+    public int SignalStart(int noRepeatLength)
+    {
+
+        
+
+        string signal = File.ReadAllText(@"C:\Users\szymo\Desktop\InputDay6.txt");
+
+        Queue<char> letters = new Queue<char>();
+        Queue<char> distinctLetters = new Queue<char>();
+
+        for (int i = 0; i < signal.Length; i++)
+        {
+            letters.Enqueue(signal[i]);
+            
+            distinctLetters = new Queue<char>(letters.Distinct());
+            if(distinctLetters.Count == letters.Count && letters.Count >= noRepeatLength)
+            {
+                return i + 1;
+            }
+            
+            if (letters.Count >= noRepeatLength) 
+            {
+                letters.Dequeue();
+            }
+
+        }
+        
+        return 0;
+    }
+}
+
