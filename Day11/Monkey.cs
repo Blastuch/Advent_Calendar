d1e72b53d30808804423055f9f1b4d95abfe14af
Blastuch <87999681+Blastuch@users.noreply.github.com>
2022-12-12 23:41:54 +0100

    Add files via upload

---
+++ Day11/Monkey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Monkey
+    {
+        public Queue<int> items = new Queue<int>();
+        public string operationSign = "";
+        public string operationValue;
+        public int testValue = 166;
+        public int trueThrow = 266;
+        public int falseThrow = 366;
+        public int monkeyActivity = 0;
+    }
+
